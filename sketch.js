bc3226cfa36bafbeff9436c12dd7e84bcdb3423d
lucasmartinsribeiro <lucasmartins0498@gmail.com>
2020-06-23 23:13:40 -0300

    Aula 1

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let imagemCenario
+let imagemPersonagem
+let cenario
+let somDoJogo
+let personagem
+
+function preload(){
+  imagemCenario = loadImage('imagens/cenario/floresta.png')
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png')
+  somDoJogo = loadSound('sons/trilha_jogo.mp3')
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  cenario = new Cenario(imagemCenario, 3)
+  personagem = new Personagem(imagemPersonagem)
+  frameRate(40)
+  somDoJogo.loop()//play executa um avez, loop executa várias vezes
+}
+
+function draw() {
+  cenario.exibe()
+  cenario.move()
+  personagem.exibe()
+}
+
